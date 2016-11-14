4b88f98bfd7970d94cf4b7cfd2a40e1f761600a7
jona8690 <jona8690@edu.eal.dk>
2016-11-14 11:45:16 +0100

    Database 3 Exercise 2

--- Programming/ExDB03_EX02.sql
+++ Programming/ExDB03_EX02.sql
@@ -1,19 +1,27 @@
+DELETE class_PET;
+DELETE class_PET_OWNER;
+
+DROP TABLE class_PET;
+DROP TABLE class_PET_OWNER;
+
 CREATE TABLE class_PET_OWNER (
-	OwnerID			Int			PRIMARY KEY,
-	OwnerLastName	Char(25)	NOT NULL,
-	OwnerFirstName	Char(25)	NOT NULL,
-	OwnerPhone		Char(15)	NULL,
-	OwnerEmail		Varchar		NOT NULL UNIQUE
+	OwnerID			Int				NOT NULL IDENTITY(1,1),
+	OwnerLastName	NVarChar(25)	NOT NULL,
+	OwnerFirstName	NVarChar(25)	NOT NULL,
+	OwnerPhone		NVarChar(25)	NULL,
+	OwnerEmail		NVarChar(50)		NOT NULL UNIQUE,
+	CONSTRAINT		PETOWNR_PK	PRIMARY KEY(OwnerID)
 );
 
 CREATE TABLE class_PET (
-	PetID			Int			PRIMARY KEY,
-	PetName			Char(25)	NOT NULL,
-	PetType			Char(25)	NOT NULL,
-	PetBreed		Char(25)	NOT NULL,
+	PetID			Int			NOT NULL IDENTITY(1,1),
+	PetName			NVarChar(25)	NOT NULL,
+	PetType			NVarChar(25)	NOT NULL,
+	PetBreed		NVarChar(25)	NOT NULL,
 	PetDOB			Date		NULL,
 	PetWeight		Float		NOT NULL,
-	OwnerID			Int			NOT NULL
+	OwnerID			Int			NOT NULL,
+	CONSTRAINT		PET_PK		PRIMARY KEY(PetID),
 	CONSTRAINT		PET_POWNER_FK	FOREIGN KEY(OwnerID)
 					REFERENCES class_PET_OWNER(OwnerID)
 );
