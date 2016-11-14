7d1da12685646f3d91039d8838ffd992d6619e0c
jona8690 <jona8690@edu.eal.dk>
2016-11-14 13:45:49 +0100

    Did ex2 again, sorry Rox :P

--- Programming/ExDB04_Ex02.sql
+++ Programming/ExDB04_Ex02.sql
@@ -1,24 +1,23 @@
---3.15
-SELECT PetBreed 
+-- 3.15
+SELECT PetBreed
 FROM class_PET;
 
---3.16
-SELECT Count(*)
+-- 3.16
+SELECT COUNT(*)
 FROM class_PET;
 
---3.17
-SELECT OwnerLastName, OwnerFirstName, OwnerEmail
-FROM class_PET_Owner
+-- 3.17
+SELECT OwnerLastName, OwnerLastName, OwnerEmail
+FROM class_PET_OWNER
 WHERE OwnerPhone IS NULL;
 
---3.18
+-- 3.18
 SELECT PetBreed, PetType, PetDOB
 FROM class_PET
 WHERE PetType = 'Dog';
 
-
 --3.19
-SELECT *
+SELECT PetID, PetName, PetType, PetBreed, PetDOB, PetWeight, OwnerID
 FROM class_PET;
 
 --3.20
@@ -32,6 +31,9 @@ FROM class_PET
 WHERE PetType = 'Dog' AND PetBreed = 'Std. Poodle';
 
 --3.22
-SELECT PetName, PetBreed,PetType
+SELECT PetName, PetBreed, PetType
 FROM class_PET
-WHERE PetType NOT IN ('Cat','Dog','Fish');
+--WHERE PetType != 'Cat' AND PetType != 'Dog' AND PetType != 'Fish';
+WHERE PetType NOT IN('Cat', 'Dog', 'Fish');
+
+
