25b00cba36bc4d2caa96b93ccd6044cc98be9681
jona8690 <jona8690@edu.eal.dk>
2016-11-15 13:33:19 +0100

    PrettyHair01 Ex01 - b

---
+++ Programming/PrettyHair/_CODE/PrettyHair/Core/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core {
+	public class Order {
+		public int ID { get; set; }
+		public DateTime Date { get; set; }
+		public DateTime DeliveryDate { get; set; }
+		public bool Picked { get; set; }
+
+		public Order(int ID, DateTime DeliveryDate) {
+			this.ID = ID;
+			this.Date = DateTime.Now;
+			this.DeliveryDate = DeliveryDate;
+			this.Picked = false;
+		}
+
+	}
+}
---
+++ Programming/PrettyHair/_CODE/PrettyHair/Core/OrderRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core {
+	class OrderRepository {
+		private Dictionary<int, Order> Orders;
+		private int OrderNumbers = 0;
+
+		public int StartOrder(DateTime DeliveryDate) {
+			int NewOrderID = this.GetNewID();
+			Order order = new Order(NewOrderID, DeliveryDate);
+			this.SaveOrder(order);
+			return NewOrderID;
+		}
+
+		private void SaveOrder(Order order) {
+			this.Orders.Add(order.ID, order);
+		}
+
+		private int GetNewID() {
+			return ++this.OrderNumbers;
+		}
+	}
+}
