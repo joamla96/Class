a64a3bf644bfd945e215453b93c2f73fa69ea782
jona8690 <jona8690@edu.eal.dk>
2016-11-21 09:25:45 +0100

    3.36 /w Alt

--- Programming/ExDB05_EX01.sql
+++ Programming/ExDB05_EX01.sql
@@ -5,4 +5,33 @@ WHERE OwnerID IN (
 	SELECT OwnerID
 	FROM class_PET
 	WHERE PetType = 'Cat'
-)
+);
+
+-- 3.35 Alternative Approach with Join
+SELECT DISTINCT OwnerFirstName, OwnerLastName, OwnerEmail
+FROM class_PET_OWNER AS o
+JOIN class_PET  AS p ON p.OwnerID = o.OwnerID
+WHERE p.PetType = 'Cat';
+
+
+-- 3.36
+SELECT OwnerFirstname, OwnerLastname, OwnerEmail 
+FROM class_PET_OWNER
+WHERE OwnerID IN (
+	SELECT OwnerID
+	FROM class_PET
+	WHERE	PetType = 'Cat'
+	AND		PetName = 'Teddy'	
+);
+
+-- 3.36 Alternative Approach with Join
+SELECT DISTINCT OwnerFirstName, OwnerLastName, OwnerEmail
+FROM class_PET_OWNER AS o
+JOIN class_PET  AS p ON p.OwnerID = o.OwnerID
+WHERE	p.PetType = 'Cat'
+AND		p.PetName = 'Teddy';
+
+
+
+
+
