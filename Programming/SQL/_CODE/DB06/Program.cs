b293d5f88b72f9b45d5927ea5533750869c4064e
jona8690 <jona8690@edu.eal.dk>
2016-11-22 13:09:02 +0100

    Ex1 Doe

--- Programming/SQL/_CODE/DB06/Program.cs
+++ Programming/SQL/_CODE/DB06/Program.cs
@@ -19,7 +19,11 @@ namespace DB06 {
 				Console.WriteLine("Please Select your Operation:\n");
 				Console.WriteLine("1. Insert new Pet");
 				Console.WriteLine("2. Show All Pets");
-				Console.WriteLine("3. Insert new Pet OWner");
+				Console.WriteLine("3. Insert new Pet Owner");
+				Console.WriteLine("4. Search Owner by Lastname");
+				Console.WriteLine("5. Search Owner by Email");
+				Console.WriteLine("6. Get All Owners");
+				Console.WriteLine("7. Get All Owners with Their Pets");
 
 				Console.WriteLine("\nInsert Option (Eg. 1):");
 				string Input = Console.ReadLine();
@@ -28,6 +32,10 @@ namespace DB06 {
 					case "1": MenuInsertPet(); break;
 					case "2": MenuShowAllPets(); break;
 					case "3": MenuInsertNewOwner(); break;
+					case "4": MenuSearchOwnerByLastname(); break;
+					case "5": MenuSearchOwnerByEmail(); break;
+					case "6": MenuGetAllOwners(); break;
+					case "7": MenuGetAllOwnersPets(); break;
 
 					case "x":
 					case "X": MenuExit(); break;
@@ -36,7 +44,169 @@ namespace DB06 {
 			}
 		}
 
+		private void MenuGetAllOwnersPets() {
+			Console.Clear();
+			Console.WriteLine("Write Owner ID:");
+			string SearchID = Console.ReadLine();
+
+			using (SqlConnection DB = new SqlConnection(ConnectionArgs)) {
+				try {
+					DB.Open();
+
+					SqlCommand cmd = new SqlCommand("usp_GetInfomation", DB);
+					cmd.CommandType = CommandType.StoredProcedure;
+
+					cmd.Parameters.Add(new SqlParameter("OwnerID", SearchID));
+
+					SqlDataReader reader = cmd.ExecuteReader();
+
+					Console.WriteLine("Name		Pet Name	Pet Type	Pet Breed	Life Expectancy");
+					if (reader.HasRows) {
+						while (reader.Read()) {
+							string Name = reader["OwnerName"].ToString();
+							string PetName = reader["PetName"].ToString();
+							string PetType = reader["PetType"].ToString();
+							string PetBreed = reader["PetBreed"].ToString();
+							string AvgLife = reader["AverageLifeExpectancy"].ToString();
+
+							Console.Write(Name + "\t" + PetName + "\t\t"  + PetType + "\t\t" + PetBreed + "\t" + AvgLife + "\n");
+						}
+					} else {
+						Console.WriteLine("No Records Found.");
+					}
+
+				} catch (SqlException e) {
+					Console.WriteLine("Error: " + e.Message);
+				} finally {
+					DB.Close();
+				}
+
+
+				Console.WriteLine("\n\nClick any key, to return to menu.");
+				Console.ReadKey();
+			}
+		}
+
+		private void MenuGetAllOwners() {
+			using (SqlConnection DB = new SqlConnection(ConnectionArgs)) {
+				try {
+					DB.Open();
+
+					SqlCommand cmd = new SqlCommand("usp_GetOwners", DB);
+					cmd.CommandType = CommandType.StoredProcedure;
+
+					SqlDataReader reader = cmd.ExecuteReader();
+
+					Console.WriteLine("ID	Name		Phone	Email");
+					if (reader.HasRows) {
+						while (reader.Read()) {
+							string ID = reader["OwnerID"].ToString();
+							string Lastname = reader["OwnerLastName"].ToString();
+							string Firstname = reader["OwnerFirstName"].ToString();
+							string Phone = reader["OwnerPhone"].ToString();
+							string Email = reader["OwnerEmail"].ToString();
+							
+							Console.Write(ID + "\t" + Firstname + " " + Lastname + "\t" + Phone + "\t" + Email + "\n");
+						}
+					} else {
+						Console.WriteLine("No Records Found.");
+					}
+
+				} catch (SqlException e) {
+					Console.WriteLine("Error: " + e.Message);
+				} finally {
+					DB.Close();
+				}
+
+
+				Console.WriteLine("\n\nClick any key, to return to menu.");
+				Console.ReadKey();
+			}
+		}
+
+		private void MenuSearchOwnerByEmail() {
+			Console.Clear();
+
+			Console.WriteLine("Email to Search by:");
+			string SearchEmail = Console.ReadLine();
+
+			Console.WriteLine("Firstname to Search by:");
+			string SearchName = Console.ReadLine();
+
+			using (SqlConnection DB = new SqlConnection(ConnectionArgs)) {
+				SqlCommand cmd = new SqlCommand("usp_GetOwnerByEmail", DB);
+				cmd.CommandType = CommandType.StoredProcedure;
+				cmd.Parameters.Add(new SqlParameter("Email", SearchEmail));
+				cmd.Parameters.Add(new SqlParameter("FirstName", SearchName));
+
+				try {
+					DB.Open();
+					SqlDataReader reader = cmd.ExecuteReader();
+
+					Console.WriteLine("ID	Name		Phone		Email");
+					if (reader.HasRows) {
+						while (reader.Read()) {
+							string ID = reader["OwnerID"].ToString();
+							string Firstname = reader["OwnerFirstName"].ToString();
+							string Lastname = reader["OwnerLastName"].ToString();
+							string Phone = reader["OwnerPhone"].ToString();
+							string Email = reader["OwnerEmail"].ToString();
+
+							Console.Write(ID + "\t" + Firstname + " " + Lastname + "\t" + Phone + "\t" + Email + "\n");
+						}
+					} else {
+						Console.WriteLine("No Records Found.");
+					}
+				} catch (SqlException e) {
+					Console.WriteLine("Error: " + e.Message);
+				} finally {
+					DB.Close();
+				}
+			}
+			Console.ReadKey();
+		}
+
+		private void MenuSearchOwnerByLastname() {
+			Console.Clear();
+
+			Console.WriteLine("Lastname to Search by:");
+			string SearchInput = Console.ReadLine();
+
+			using (SqlConnection DB = new SqlConnection(ConnectionArgs)) {
+				SqlCommand cmd = new SqlCommand("usp_GetOwnerByLastName", DB);
+				cmd.CommandType = CommandType.StoredProcedure;
+				cmd.Parameters.Add(new SqlParameter("LastName", SearchInput));
+
+				try {
+					DB.Open();
+					SqlDataReader reader = cmd.ExecuteReader();
+
+					Console.WriteLine("ID	Name		Phone		Email");
+					if (reader.HasRows) {
+						while (reader.Read()) {
+							string ID = reader["OwnerID"].ToString();
+							string Firstname = reader["OwnerFirstName"].ToString();
+							string Lastname = reader["OwnerLastName"].ToString();
+							string Phone = reader["OwnerPhone"].ToString();
+							string Email = reader["OwnerEmail"].ToString();
+
+							Console.Write(ID + "\t" + Firstname + " " + Lastname + "\t" + Phone + "\t" + Email + "\n");
+						}
+					} else {
+						Console.WriteLine("No Records Found.");
+					}
+				} catch (SqlException e) {
+					Console.WriteLine("Error: " + e.Message);
+				} finally {
+					DB.Close();
+				}
+			}
+			Console.ReadKey();
+		}
+
 		private void MenuInsertNewOwner() {
+			Console.Clear();
+
 			Console.WriteLine("Write Firstname:");
 			string Firstname = Console.ReadLine();
 
