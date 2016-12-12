d678a1cacd76b8a38258623e6440c68f60ad081c
jona8690 <jona8690@edu.eal.dk>
2016-12-12 08:38:51 +0100

    Implemented GetInput method & started with validator system

--- Programming/PrettyHair/_CODE/PrettyHair/UserInterface_CLI/Program.cs
+++ Programming/PrettyHair/_CODE/PrettyHair/UserInterface_CLI/Program.cs
@@ -1,6 +1,37 @@
-﻿namespace UserInterface_CLI {
+﻿using System;
+
+namespace UserInterface_CLI {
 	class Program {
 		static void Main(string[] args) {
+			Program a = new Program();
+			a.Run();
+		}
+
+		private void Run() {
+
+		}
+
+		private string GetInput(string rule = "", string err = "") {
+			if (err != "") {
+				Console.WriteLine("\n" + err);
+			}
+			string Input = Console.ReadLine();
+			Validator Valid = new Validator();
+			switch (rule) {
+				case "text":
+					if (!Valid.text(Input)) return GetInput(rule, "Text Only!");
+					break;
+
+				case "number":
+					if (!Valid.number(Input)) return GetInput(rule, "Numbers Only!");
+					break;
+
+
+				default:
+					if (rule != "") throw new Exception("Invalid Validation Parameter");
+					break;
+			}
+			return Input;
 		}
 	}
 }
---
+++ Programming/PrettyHair/_CODE/PrettyHair/UserInterface_CLI/Validator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInterface_CLI {
+	class Validator {
+		public bool text(string input) {
+			return true; //TODO: Implement
+		}
+
+		public bool number(string input) {
+			return true; //TODO: Implement
+		}
+	}
+}
